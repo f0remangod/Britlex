2d301329332b2a7e323bf71698af45a4b2b582da
f0remangod <probill27@gmail.com>
2022-07-19 00:05:51 +0300

    script

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,13 @@
 (() => {
-  const menuBtnRef = document.querySelector("[data-menu-button]");
+  const menuItemRef = document.querySelector("[data-menu-item]");
   const mobileMenuRef = document.querySelector("[data-menu]");
+  const menuBtnRef = document.querySelector("[data-menu-button]");
 
+  menuItemRef.addEventListener("click", () => {
+    mobileMenuRef.classList.toggle("is-open");
+    document.body.classList.toggle("menu-open");
+    menuBtnRef.classList.toggle("is-open");
+  });
   menuBtnRef.addEventListener("click", () => {
     const expanded =
       menuBtnRef.getAttribute("aria-expanded") === "true" || false;
